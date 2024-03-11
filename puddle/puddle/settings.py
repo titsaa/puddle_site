7aee7bfd2e1013ba2d540e34bb39f9340cead0cd
titsaa <titsaa@example.com>
2024-03-11 15:31:09 -0400

    imodified the ALLOWED_HOSTS entries in the settings.py file to match those of the EC2 instance hosting the project

--- puddle/puddle/settings.py
+++ puddle/puddle/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-lm)%oq-nge98fr842m4h9n_5$bhrrc*+dpsk3=u(-8eykrdh2&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["54.89.55.38", "54.89.55.38"]
 
 LOGIN_URL = 'login/'
 LOGIN_REDIRECT_URL = '/'
